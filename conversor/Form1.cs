3a90af50fbf8af523d0f4c37bbaef1ada82857f6
Bolsarim <142364522+Bolsarim@users.noreply.github.com>
2024-06-12 21:06:10 -0300

    Adicionar arquivos de projeto.

---
+++ conversor/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace conversor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double celsius = double.Parse(textCelsius.Text);
+            double fahrenheint = (celsius * 9 / 5) + 32;
+            textFahrenheit.Text = fahrenheint.ToString();
+        }
+    }
+}
